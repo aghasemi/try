9a6c536cf862e1d55010cebea355eca87e85503e
root <ghasemi.a.ir@gmail.com>
2022-01-10 08:40:25 +0000

    Remove old functions

--- Try.java
+++ Try.java
@@ -1,8 +1,7 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-// //DEPS <dependency1> <dependency2>
 //JAVA 17
-//GAV com.github.aghasemi:try
-package com.github.aghasemi.jry;
+//GAV com.github.aghasemi:try4j
+package com.github.aghasemi.try4j; // The IDE will complain about this. Ignore it.
 
 import static java.lang.System.*;
 
@@ -14,53 +13,39 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Try {
 
     public interface ExceptionFreeFunction<T> {
-        public T run() throws Exception;
+        public T get() throws Exception;
     }
 
-    public static<U> Optional<U> doTry(ExceptionFreeFunction f, boolean nullIsValid, AtomicReference exception)
+    public interface ExceptionFreeVoidFunction {
+        public void run() throws Exception;
+    }
+
+    public static<U> Optional<U> get(ExceptionFreeFunction f, boolean emptyIfNull)
     {
         try {
-            U result = (U) f.run();
-            if(exception!=null) exception.set(null);
-            return !nullIsValid && result==null ? Optional.empty() :  Optional.of(result);
+            U result = (U) f.get();
+            return emptyIfNull && result==null ? Optional.empty() :  Optional.of(result);
         } catch (Exception e) {
-            if(exception!=null) exception.set(e);
             return Optional.empty();
         }
     }
 
-    public static<U> U doTry(ExceptionFreeFunction f, U defaultValue, boolean nullIsValid, AtomicReference exception)
+    public static<U> Optional<U> get(ExceptionFreeFunction f)
+    {
+        return get(f, false);
+    }
+
+    public static void run(ExceptionFreeVoidFunction f)
     {
         try {
-            U result = (U) f.run();
-            if(exception!=null) exception.set(null);
-            return !nullIsValid && result==null ? defaultValue :  result;
+            f.run();
         } catch (Exception e) {
-            if(exception!=null) exception.set(e);
-            return defaultValue;
         }
     }
 
-    public static<U> Optional<U> doTry(ExceptionFreeFunction f, boolean nullIsValid)
-    {
-        return doTry(f, nullIsValid, null);
-    }
+    
 
-    public static<U> U doTry(ExceptionFreeFunction f, U defaultValue, boolean nullIsValid)
-    {
-        return doTry(f, defaultValue, nullIsValid, null);
-    }
-
-
-    public static<U> Optional<U> doTry(ExceptionFreeFunction f)
-    {
-        return doTry(f, true);
-    }
-
-    public static<U> U doTry(ExceptionFreeFunction f, U defaultValue)
-    {
-        return doTry(f, defaultValue, true);
-    }
+    
 
     public static void main(String... args) {
         out.println("Hello World");
